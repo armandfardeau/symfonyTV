d6ee17ed28722a58d9b10c442f27228da10d1db8
Armand Fardeau <fardeauarmand@gmail.com>
2017-02-15 10:18:38 +0100

    add Main/Front/back/cms bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,6 +16,10 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
+            new SfWebApp\MainBundle\SfWebAppMainBundle(),
+            new SfWebApp\BackOfficeBundle\SfWebAppBackOfficeBundle(),
+            new SfWebApp\FrontOfficeBundle\SfWebAppFrontOfficeBundle(),
+            new SfWebApp\CmsBundle\SfWebAppCmsBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
