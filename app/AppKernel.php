58f65e9738b2fb612e61a22d2c8b73ea3922d5a7
Armand Fardeau <fardeauarmand@gmail.com>
2017-02-15 12:09:02 +0100

    HOTFIX: propre surpress of AppBudle in AppKernel.php

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -15,7 +15,6 @@ class AppKernel extends Kernel
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new AppBundle\AppBundle(),
             //Bundle for entities
             new SfWebApp\MainBundle\SfWebAppMainBundle(),
             //Bundle for backoffice
