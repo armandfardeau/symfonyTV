a356f248a9081df7d69eb7b9d68777d4d2ef8a29
Armand Fardeau <fardeauarmand@gmail.com>
2017-02-20 17:58:54 +0100

    fosuserbundle is working

--- src/SfWebApp/MainBundle/Entity/User.php
+++ src/SfWebApp/MainBundle/Entity/User.php
@@ -3,7 +3,6 @@
 namespace SfWebApp\MainBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
-use Doctrine\Common\Collections\ArrayCollection;
 use FOS\UserBundle\Entity\User as BaseUser;
 
 /**
@@ -30,253 +29,32 @@ class User extends BaseUser
     /**
      * @var
      *
-     * @ORM\ManyToMany(targetEntity="Group", inversedBy="users")
-     * @ORM\JoinTable(name="users_groups",
-     *      joinColumns={@ORM\JoinColumn(name="user_id", referencedColumnName="id")},
-     *      inverseJoinColumns={@ORM\JoinColumn(name="group_id", referencedColumnName="id")}
-     * )
-     */
-    protected $groups;
-
-    /**
-     * @var
-     *
      * @ORM\Column(name="gender", type="string", length=255, nullable=true)
      */
-    protected $gender;
 
     /**
-     * @var
-     *
-     * @ORM\Column(name="firstname", type="string", length=255, nullable=true)
-     */
-    protected $firstname;
-
-    /**
-     * @var
-     *
-     * @ORM\Column(name="lastname", type="string", length=255, nullable=true)
-     */
-    protected $lastname;
-
-    /**
-     * @var
-     *
-     * @ORM\Column(name="address", type="text", nullable=true, nullable=true)
-     */
-    protected $address;
-
-    /**
-     * @var
-     *
-     * @ORM\Column(name="zip_code", type="string", length=255, nullable=true)
-     */
-    protected $zipCode;
-
-    /**
-     * @var
-     *
-     * @ORM\Column(name="city", type="string", length=255, nullable=true)
-     */
-    protected $city;
-
-    /**
-     * @var
-     *
-     * @ORM\Column(name="country", type="string", length=255, nullable=true)
-     */
-    protected $country;
-
-    /**
-     * @var
-     *
-     * @ORM\Column(name="phone", type="string", length=255, nullable=true)
-     */
-    protected $phone;
-
-    /**
-     * Constructor
+     * User constructor.
      */
     public function __construct()
     {
         parent::__construct();
-        $this->groups = new ArrayCollection();
-    }
 
-    /**
-     * @param mixed $gender
-     */
-    public function setGender($gender)
-    {
-        $this->gender = $gender;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getGender()
-    {
-        return $this->gender;
-    }
-
-    /**
-     * @param mixed $firstname
-     */
-    public function setFirstname($firstname)
-    {
-        $this->firstname = $firstname;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getFirstname()
-    {
-        return $this->firstname;
-    }
-
-    /**
-     * @param mixed $lastname
-     */
-    public function setLastname($lastname)
-    {
-        $this->lastname = $lastname;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getLastname()
-    {
-        return $this->lastname;
-    }
-
-
-
-    /**
-     * @param mixed $address
-     */
-    public function setAddress($address)
-    {
-        $this->address = $address;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getAddress()
-    {
-        return $this->address;
-    }
-
-    /**
-     * @param mixed $city
-     */
-    public function setCity($city)
-    {
-        $this->city = $city;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getCity()
-    {
-        return $this->city;
-    }
-
-    /**
-     * @param mixed $country
-     */
-    public function setCountry($country)
-    {
-        $this->country = $country;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getCountry()
-    {
-        return $this->country;
-    }
-
-    /**
-     * @param int $id
-     */
-    public function setId($id)
-    {
-        $this->id = $id;
     }
 
     /**
      * @return int
      */
-    public function getId()
+    public function getId(): int
     {
         return $this->id;
     }
 
     /**
-     * @param mixed $zipCode
+     * @param int $id
      */
-    public function setZipCode($zipCode)
+    public function setId(int $id)
     {
-        $this->zipCode = $zipCode;
+        $this->id = $id;
     }
 
-    /**
-     * @return mixed
-     */
-    public function getZipCode()
-    {
-        return $this->zipCode;
-    }
-
-    /**
-     * @param mixed $phone
-     */
-    public function setPhone($phone)
-    {
-        $this->phone = $phone;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getPhone()
-    {
-        return $this->phone;
-    }
-
-    /**
-     * @param $group
-     * @return $this
-     */
-    public function addGoup($group)
-    {
-        $this->groups[] = $group;
-        $group->setUser($this);
-
-        return $this;
-    }
-
-    /**
-     * @param $groups
-     */
-    public function setGroups($groups)
-    {
-        $this->groups->clear();
-        foreach ($groups as $group) {
-            $this->addGroup($group);
-        }
-    }
-
-    /**
-     * @return ArrayCollection
-     */
-    public function getGroups()
-    {
-        return $this->groups;
-    }
 }
